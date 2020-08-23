27899ea94041bdf57d4d977b91cefdaa7d1e85dd
thelittlecitizen13 <thelittlecitizen13@gmail.com>
2020-08-23 17:34:17 +0300

    IFacebookClient interface added

---
+++ DolphinFacebook/IFacebookClient.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DolphinFacebook
+{ 
+    public interface IFacebookClient
+    {
+        event Action<string> NewWallPost;
+
+        void WriteNewWallPost(string wallPost);
+
+        void Subscribe(IFacebookClient publisher);
+
+        void Unsubscribe(IFacebookClient publisher);
+    }
+
+}
