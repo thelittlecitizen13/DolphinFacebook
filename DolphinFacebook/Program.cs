901118a52d9f640cc05196072d4a128dbcf72277
thelittlecitizen13 <thelittlecitizen13@gmail.com>
2020-08-23 19:55:51 +0300

    Tested

--- DolphinFacebook/Program.cs
+++ DolphinFacebook/Program.cs
@@ -4,7 +4,19 @@
     {
         static void Main(string[] args)
         {
-            
+            Display d1 = new Display("Assa");
+            Display d2 = new Display("Yuval");
+            Display d3 = new Display("Idan");
+            FacebookClientFactory factory = new FacebookClientFactory();
+            DolphinsFacebookClient AssaWall = (DolphinsFacebookClient)factory.CreateClient(d1);
+            DolphinsFacebookClient YuvalWall = (DolphinsFacebookClient)factory.CreateClient(d2);
+            DolphinsFacebookClient IdanWall = (DolphinsFacebookClient)factory.CreateClient(d3);
+            AssaWall.Subscribe(YuvalWall);
+            AssaWall.Subscribe(IdanWall);
+            IdanWall.Subscribe(YuvalWall);
+            IdanWall.Subscribe(AssaWall);
+            YuvalWall.WriteNewWallPost("Mamas enough with the number 6!!!!");
+            AssaWall.WriteNewWallPost("Enough with those avatars on facebook! Where are the indian people who build pools??");
         }
     }
 }
