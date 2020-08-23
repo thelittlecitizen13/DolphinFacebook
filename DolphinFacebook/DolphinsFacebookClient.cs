4492287ce73d1b483d2c1635b644760f35e5228b
thelittlecitizen13 <thelittlecitizen13@gmail.com>
2020-08-23 17:38:02 +0300

    DolphinsFacebookClient class added

---
+++ DolphinFacebook/DolphinsFacebookClient.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace DolphinFacebook
+{
+    public class DolphinsFacebookClient : IFacebookClient
+    {
+        public event Action<string> NewWallPost;
+
+        public void Subscribe(IFacebookClient publisher)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Unsubscribe(IFacebookClient publisher)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void WriteNewWallPost(string wallPost)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
